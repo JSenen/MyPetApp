7d752a4feb4347993bbffe0c512c9d637de6c98b
Juan Senen <juansenen@hotmail.com>
2022-03-28 23:24:18 +0200

    Test creacion facturas

--- src/test/java/FacturasTest.java
+++ src/test/java/FacturasTest.java
@@ -1,10 +1,21 @@
+import com.juansenen.mypetapp.domain.Facturas;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+
 public class FacturasTest {
+    private String concepto = "Prueba de prueba";
+    private float iva = 0.21f;
+    private boolean pagado = true;
+    private float precio = 100;
+
 
     @Test
     public void crearFacturaTest(){
-
+        Facturas factura = new Facturas(concepto, iva, precio, pagado);
+        Facturas factura2= new Facturas(concepto, iva, precio, pagado);
+        assertEquals(factura2.getIdFactura(),2); //Comparamos que se tienen que haber creado y ser la numero 2
     }
 
     @Test
