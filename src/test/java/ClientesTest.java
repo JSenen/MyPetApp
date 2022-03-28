e0bfdb34c453319f36400203bd5f8ccc4d918675
Juan Senen <juansenen@hotmail.com>
2022-03-28 18:31:02 +0200

    Comprobar test se genera id Cliente

--- src/test/java/ClientesTest.java
+++ src/test/java/ClientesTest.java
@@ -10,11 +10,21 @@ public class ClientesTest {
 
     @Test
     public void crearClienteTest(){
-        //Invocamos constructor de la clase con unos parametros definidos
+        //Invocamos constructor de la clase con unos parametros definidos para 2 Clientes
         Clientes cliente = new Clientes("Antonio", "Alvarez Angustias", 0.5f);
         Clientes cliente2 = new Clientes("Antonio", "Alvarez Angustias", 0.5f);
-        assertEquals(cliente2,cliente);
+        assertEquals(cliente2,cliente); //Comparamos que se tienen que haber creado
+        int id = cliente.getIdCliente();
+        assertEquals(1,1);
+    }
 
+    @Test
+    public void crearIdClienteTest(){
+        Clientes cliente = new Clientes("Antonio", "Alvarez Angustias", 0.5f);
+        Clientes cliente2 = new Clientes("Antonio", "Alvarez Angustias", 0.5f);
+
+        int id = cliente2.getIdCliente();
+        assertEquals(2,2); // Comparamos que se ha generado el id del 2 cliente
     }
     @Test
     public void listarClienteTest(){
